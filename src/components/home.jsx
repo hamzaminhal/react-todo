ad88201fc13e086ee05ace41ee1699cda9392bde
hamzaminhal <hamzaminhal2@gmail.com>
2025-10-04 14:05:06 +0500

    todo list logics built

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,12 +4,37 @@ import "./home.css";
 function TodoList() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const keyDown = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
+  // Add Task Function
 
   function addTask() {
     let temp = [...todos];
     temp.push(input);
     setTodos(temp);
     setInput("");
+  }
+
+  // Edit Function
+
+  function edit(idx) {
+    const newTodos = [...todos];
+    const newTask = prompt("Enter New Task", todos[idx]);
+    newTodos.splice(idx, 1, newTask);
+    setTodos(newTodos);
+  }
+
+  // Delete Function
+
+  function dlt(idx) {
+    const newTodos = [...todos];
+    newTodos.splice(idx, 1);
+    // console.log(todos);
+    setTodos(newTodos);
   }
 
   return (
@@ -43,6 +68,7 @@ function TodoList() {
               placeholder="Add a new task"
               //   aria-label="Add task"
               value={input}
+              onKeyDown={keyDown}
               onChange={(e) => {
                 setInput(e.target.value);
               }}
@@ -56,8 +82,48 @@ function TodoList() {
         <div className="list">
           {todos.map((todo, index) => {
             return (
-              <div key={index}>
-                {index + 1}. {todo}
+              <div key={index} className="listItem">
+                <span className="listDesc">
+                  {index + 1}. {todo}
+                </span>
+                {/* Edit SVG */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  width="20px"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                  onClick={() => {
+                    edit(index);
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+                {/* Delete SVG */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  width="20px"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                  onClick={() => {
+                    dlt(index);
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
               </div>
             );
           })}
